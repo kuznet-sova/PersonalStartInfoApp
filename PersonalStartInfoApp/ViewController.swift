69f0bd5ff919a2b09e5b9758f55114cda47f377a
kuznet.i.sova <kuznet.i.sova@gmail.com>
2020-05-29 00:47:32 +0300

    first screen

---
+++ PersonalStartInfoApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  PersonalStartInfoApp
+//
+//  Created by Ирина Кузнецова on 28.05.2020.
+//  Copyright © 2020 Irina Kuznetsova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var usernameTextFild: UITextField!
+    @IBOutlet var passwordTextFild: UITextField!
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+
+    @IBAction func usernameFild() {
+    }
+    @IBAction func passwordFild() {
+    }
+    
+    @IBAction func loginButton() {
+    }
+    @IBAction func forgotUsernameButton() {
+    }
+    @IBAction func forgetPasswordButton() {
+    }
+}
+
